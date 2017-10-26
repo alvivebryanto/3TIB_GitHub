b991455cb3db70c18670084405d3c37456358d6a
alvivebryanto <Alvi15ti@mahasiswa.pcr.ac.id>
2017-10-26 09:52:16 +0700

    updated

---
+++ 3TIB_GitHub/pertama/cobalabel.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3TIB_GitHub.pertama
+{
+    public partial class cobalabel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label1 .Text = "coba"
+        }
+    }
+}
